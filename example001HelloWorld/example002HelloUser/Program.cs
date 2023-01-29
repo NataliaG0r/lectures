0e60165d732eebb8ffdff0e0bd3033c7d9beb0f6
Natalia_Go <gorshana@gmail.com>
2023-01-29 23:06:18 +0300

    example3 addition problem

--- example001HelloWorld/example002HelloUser/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-
-Console.WriteLine("What is your name?");
-string username = Console.ReadLine();
-Console.Write ("hello," );
-Console.Write ( username);
-
